d782371fd0c931e469e24381babc5d0f7cfed978
David <david.poslt@moneta.cz>
2017-05-05 15:40:17 +0200

    fix comments

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-<<<<<<< HEAD
+#!/usr/bin/env python
+__author__ = 'David Poslt'
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,9 +15,5 @@ def rewrite_index():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=5000, debug=True)
-=======
-#!/usr/bin/env python
-__author__ = 'David Poslt'
 
 
->>>>>>> 2f874f82a7a51707149ea4f613353fd0e7ef529c
